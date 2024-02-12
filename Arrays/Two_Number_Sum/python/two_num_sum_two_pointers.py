5a248bfddf2d016ad566fc7b030a42c3add7614c
DDaniel-Dev <ddaniel.atx@gmail.com>
2024-02-11 22:57:17 -0600

    Arrays(Two Number Sum)(Python): Two Pointers Solution

---
+++ Arrays/Two_Number_Sum/python/two_num_sum_two_pointers.py
@@ -0,0 +1,14 @@
+def two_num_sum_two_pointers(array, target_sum):
+    array.sort()
+    left, right = 0, len(array) - 1
+
+    while left < right:
+        current_sum = array[left] + array[right]
+        if current_sum == target_sum:
+            return [array[left], array[right]]
+        elif current_sum < target_sum:
+            left += 1
+        else:
+            right -= 1
+    
+    return []
