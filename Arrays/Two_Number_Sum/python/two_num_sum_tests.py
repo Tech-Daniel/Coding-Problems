4691646b368201cd7b110489ef3381ead25c26c9
DDaniel-Dev <ddaniel.atx@gmail.com>
2024-02-13 23:55:22 -0600

    Arrays(Two Number Sum)(Python): Test 1

--- Arrays/Two_Number_Sum/python/two_num_sum_tests.py
+++ Arrays/Two_Number_Sum/python/two_num_sum_tests.py
@@ -1,5 +1,15 @@
+import two_num_sum_brute_force
+import two_num_sum_sets
+import two_num_sum_two_pointers
+
 import unittest
 
 class TestProgram(unittest.TestCase):
-    
+    def test_case_1(self):
+        array = [3, 5, -4, 8, 11, 1, -1, 6]
+        target_sum = 10
+        brute_force = two_num_sum_brute_force(array, target_sum)
+        self.assertTrue(len(brute_force) == 2)
+        self.assertTrue(11 in brute_force)
+        self.assertTrue(-1 in brute_force)
 
