ebee538cac17f8c22bfd4844610eb1c674581c12
DDaniel-Dev <ddaniel.atx@gmail.com>
2024-02-28 23:45:10 -0600

    Arrays(Two Number Sum)(Python): Set Solution (updates-v1)

--- Arrays/Two_Number_Sum/python/two_num_sum_sets.py
+++ Arrays/Two_Number_Sum/python/two_num_sum_sets.py
@@ -1,27 +1,20 @@
+# TIME: O(n); where n is the numbers in the array
+# SPACE: O(n); where n is the numbers in the set()
 
 def two_num_sum_sets(array, target_sum):
-	''' nums = set()
-	for num in array:
-		potential_match = target_sum - num
-		if potential_match in nums:
-			return [potential_match, num]
-		else:
-			nums.add(potential_match)
-	return [] '''
-
 	if len(array) < 2:
 		return []
 	
 	if len(array) != len(set(array)):
 		raise ValueError("There is a repeated element in the given array!")
-
-	dict = {}
-
+	
+	nums = set()
 	for num in array:
-		potentialMatch = target_sum - num
-
-		if potentialMatch in dict:
-			return [potentialMatch, num]
+		potential_match = target_sum - num
+		if num in nums:
+			return [potential_match, num]
 		else:
-			dict[num] = "Matching Number"
-	return []
+			nums.add(potential_match)
+	return [] 
+
+	
