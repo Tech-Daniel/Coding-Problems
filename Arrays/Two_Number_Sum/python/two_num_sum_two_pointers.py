1b3f8ed40e062800e361922db3a65c0df46159fc
DDaniel-Dev <ddaniel.atx@gmail.com>
2024-03-04 23:41:00 -0600

    Arrays(Two Number Sum)(Python): Two Pointers Solution (updates-v1)

--- Arrays/Two_Number_Sum/python/two_num_sum_two_pointers.py
+++ Arrays/Two_Number_Sum/python/two_num_sum_two_pointers.py
@@ -1,20 +1,59 @@
-def two_num_sum_two_pointers(array, target_sum):
-    if len(array) < 2:
-    	return []
+def two_num_sum_two_pointers(numbers, target_sum):
+    """
+    Finds two numbers in a list that sum up to a specified target sum using the two-pointer approach.
+
+    This function first sorts the input list in-place, then uses two pointers to find a pair of numbers
+    that add up to the target sum. It returns the pair as a list if such a pair exists, or an empty list otherwise.
+
+    Time Complexity: O(n log n), where n is the number of elements in `numbers` due to the sorting operation.
+    Space Complexity: O(1), as the sorting is done in-place and only a few variables are used.
+
+    Args:
+        numbers (list): A list of integers to search through.
+        target_sum (int): The target sum to find.
+
+    Returns:
+        list: A list containing the two numbers that add up to the target_sum, or an empty list if no such pair exists.
         
-    if len(array) != len(set(array)):
-        raise ValueError("There is a repeated element in the given array!")
+    """
     
-    array.sort()
-    left, right = 0, len(array) - 1
+    validate_input(numbers, target_sum)
+
+    numbers.sort()
+    left, right = 0, len(numbers) - 1
 
     while left < right:
-        current_sum = array[left] + array[right]
+        current_sum = numbers[left] + numbers[right]
         if current_sum == target_sum:
-            return [array[left], array[right]]
+            return [numbers[left], numbers[right]]
         elif current_sum < target_sum:
             left += 1
         else:
             right -= 1
     
     return []
+
+
+def validate_input(numbers, target_sum):
+    """
+    Validates the input parameters.
+
+    Args:
+        numbers (list): The list of numbers to validate.
+        target_sum (int): The target sum to validate.
+
+    Raises:
+        TypeError: If `numbers` is not a list or `target_sum` is not an integer.
+        ValueError: If `numbers` contains duplicates or less than two elements.
+    """
+    if not isinstance(numbers, list):
+        raise TypeError(f"Expected a list, got {type(numbers)}")
+    if len(numbers) < 2:
+        raise ValueError("At least two numbers required in `numbers`")
+    if len(numbers) != len(set(numbers)):
+        raise ValueError("Duplicate integers found in `numbers`")
+    if not all(isinstance(x, int) for x in numbers):
+        raise TypeError("All elements in `numbers` must be integers")
+    if not isinstance(target_sum, int):
+        raise TypeError(f"Expected an int for `target_sum`, got {type(target_sum)}")
+    
