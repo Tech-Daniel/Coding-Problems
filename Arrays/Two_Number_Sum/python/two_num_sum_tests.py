3db1b1beb4007680131dc7cf6c00963e8880cd86
DDaniel-Dev <ddaniel.atx@gmail.com>
2024-03-15 20:46:42 -0500

    Arrays(Two Number Sum)(Python): Test 18

--- Arrays/Two_Number_Sum/python/two_num_sum_tests.py
+++ Arrays/Two_Number_Sum/python/two_num_sum_tests.py
@@ -286,6 +286,26 @@ class TestTwoNumSum(unittest.TestCase):
                 self.assertEqual(self.actual, self.expected)
                 self.test_passed = True
 
+    def test_with_duplicates_number_in_numbers_arg(self):
+        """
+        Test the two_num_sum with duplicates number from `numbers` argument.
+        """
+        self.numbers = [1, 3, 4, 5, 2, 4]
+        self.target_sum = 3
+
+        for two_num_sum_func in [two_num_sum_brute_force, 
+                                 two_num_sum_sets, 
+                                 two_num_sum_two_pointers]:
+            with self.subTest(two_num_sum_func=two_num_sum_func):
+                with self.assertRaises(ValueError) as handler:
+                    two_num_sum_func(self.numbers, self.target_sum)
+                
+                self.expected = "Duplicate integers found in `numbers`"
+                self.actual = str(handler.exception)
+                
+                self.assertEqual(self.actual, self.expected)
+                self.test_passed = True
+
     def test_with_input_null_type_for_elements_inside_numbers_arg(self):
         """
         Test the two_num_sum with null type for element in `numbers` argument.
