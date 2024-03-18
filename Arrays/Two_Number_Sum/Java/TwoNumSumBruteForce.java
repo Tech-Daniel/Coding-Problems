73a33b6cab5d42bb8dba6e4fe0c46c6e9526f544
DDaniel-Dev <ddaniel.atx@gmail.com>
2024-03-17 23:22:34 -0500

    Arrays(Two Number Sum)(Java): Brute Force Solution

---
+++ Arrays/Two_Number_Sum/Java/TwoNumSumBruteForce.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class twoNumSumBruteForce {
+  public static void main(String[] args) {
+    int [] numbers = new int[] {1, 2, 4, 5, -5, -2};
+    int targetSum = 2;
+
+    twoNumSum(numbers, targetSum);
+  }
+  
+  public static int[] twoNumSum(int[] numbers, int targetSum) {
+    for (int i = 0; i < numbers.length - 1; i++) {
+      int firstNum = numbers[i];
+      for (int j = i + 1; j < numbers.length; j++) {
+        int secondNum = numbers[j];
+        
+        if (firstNum + secondNum == targetSum) {
+          int[] result = new int[] {firstNum, secondNum};
+          System.out.println(Arrays.toString(result));
+          System.out.println("You got the answer!");
+          return new int[] {firstNum, secondNum};
+        }
+      }
+    }
+    // System.out.println(new int[0]);
+    return new int[0];
+  }
+};
