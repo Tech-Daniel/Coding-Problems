5df7db0f0a1e7fa659b493c1fe94ed219b51d51c
DDaniel-Dev <ddaniel.atx@gmail.com>
2024-03-20 23:34:58 -0500

    Arrays(Two Number Sum)(Java): Initiate Test

---
+++ Arrays/Two_Number_Sum/Java/twoNumSumTests.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import twoNumSumBruteForce;
+import twoNumSumSets;
+import twoNumSumTwoPointers;
+
+class twoNumSumTests {
+  @Test
+  public void TestCase1() {
+    int[] numbers = new int[] {3, 5, -4, 8, 11, -1};
+    int targetSum = 10;
+    int[] output = twoNumSumBruteForce(numbers, targetSum);
+
+    Utils.assertTrue(output.length == 2);
+    Utils.assertCountEqual(output, [11, -1], "Result should contain 11 and -1.");
+  }
+
+  public boolean contains(int[] output, int value) {
+    for (var el : output) {
+      if (el == value) return true;
+    }
+    return false;
+  }
+}
