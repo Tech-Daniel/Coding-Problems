f1c61ea692c2a420f5494edc08db7be336b14537
DDaniel-Dev <ddaniel.atx@gmail.com>
2024-04-07 23:42:05 -0500

    Arrays(Validate Subsequence)(Python): Test 4

--- Arrays/Validate_Subsequence/Python/validate_subsequence_tests.py
+++ Arrays/Validate_Subsequence/Python/validate_subsequence_tests.py
@@ -63,6 +63,21 @@ class Test_Validate_Subsequence(unittest.TestCase):
 				self.assertEqual(self.actual, self.expected, "Result should return True!")
 				self.test_passed = True
 
+	def test_with_duplicated_subsequence(self):
+		"""
+		Test the validate_subsequence function with `sequence` value at the Start and the End of `numbers`
+		"""
+		self.numbers = [1, 6, 1, 10, 6, 10]
+		self.sequence = [1, 6, 10]
+		for validate_subsequence_func in [	validate_subsequence_brute_force,
+											validate_subsequence_two_pointers]:
+			with self.subTest(validate_subsequence_func=validate_subsequence_func):
+				self.expected = True
+				self.actual = validate_subsequence_func(self.numbers, self.sequence)
+
+				self.assertEqual(self.actual, self.expected, "Result should return True!")
+				self.test_passed = True
+
 
 	def tearDown(self):
 		"""
